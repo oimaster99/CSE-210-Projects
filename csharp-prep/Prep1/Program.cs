d7cc0093772f72e81d59280c887f453b92bc36fa
Oobe <oimaster99@gmail.com>
2024-01-12 14:09:30 -0700

    W01 Projects completed.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
+    //Main Method
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Asks the user for their first and last names, and stores them into strings.
+        Console.Write("What is your first name?: ");
+        String firstName = Console.ReadLine();
+
+        Console.Write("What is your last name?: ");
+        String lastName = Console.ReadLine();
+
+        //Prints the first and last name of user in fashion of a James Bond quote.
+        Console.WriteLine("\nYour name is " + lastName + ", " + firstName + " " + lastName + ".");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,67 @@ using System;
 
 class Program
 {
+    //Main Method
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Asks the user for their grade in a percentage number format.
+        Console.Write("What is your grade percentage?: ");
+
+        //Converts said input into an integer.
+        int grade = Int32.Parse(Console.ReadLine());
+
+        //Initialize the variable for the letter grade.
+        String letterGrade;
+        
+        /*Checks for grade percentage, and sets the letter grade appropriately.
+          Also accounts for plus and minuses on grade.*/
+        if (grade >= 90) {
+            letterGrade = "A";
+            if (grade <= 93) {
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 80) {
+            letterGrade = "B";
+            if (grade >= 87) {
+                letterGrade += "+";
+            }
+            else if (grade <= 83) {
+
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 70) {
+            letterGrade = "C";
+            if (grade >=  77) {
+                letterGrade += "+";
+            }
+            else if (grade <= 73) {
+                letterGrade += "-";
+            }
+        }
+        else if (grade >= 60) {
+            letterGrade = "D";
+            if (grade >= 67) {
+                letterGrade += "+";
+            }
+            else if (grade <= 63) {
+                letterGrade += "-";
+            }
+        }
+        else {
+            letterGrade = "F";
+        }
+
+        /*Prints the letter grade to the console, and checks if 
+          the grade is a 70 or higher for passing. */
+        Console.WriteLine("Your grade is " + letterGrade + ".");
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratulations, You Passed!");
+        }
+        else {
+            Console.WriteLine("Sorry, you didn't pass... :(");
+        }
     }
 }
